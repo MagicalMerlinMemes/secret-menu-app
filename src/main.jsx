fedab760604fac392d971258362c023aa5d8101e
Magical Merlin <ctrevornelson@gmail.com>
2026-09-04 13:20:51 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
-const rootEl = document.getElementById('root')
-const app = (
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode><App /></React.StrictMode>,
 )
-
-if (rootEl.hasChildNodes()) {
-  ReactDOM.hydrateRoot(rootEl, app)
-} else {
-  ReactDOM.createRoot(rootEl).render(app)
-}
